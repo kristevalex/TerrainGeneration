5f678b918d334971802f9d574e7a24af62f96bb8
Alex <39654006+kristevalex@users.noreply.github.com>
2021-07-20 23:46:09 +0300

    Marching Squares

--- CaveGeneration3DVisual/Assets/Scripts/MeshGenerator.cs
+++ CaveGeneration3DVisual/Assets/Scripts/MeshGenerator.cs
@@ -1,12 +1,145 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class MeshGenerator : MonoBehaviour
 {
-    public SquareGrid squareGrid;
+	public SquareGrid squareGrid;
+	List<Vector3> vertices;
+	List<int> triangles;
 
-    public void GenerateMesh(bool[,] map, float squareSize)
+	public void GenerateMesh(bool[,] map, float squareSize)
+	{
+		squareGrid = new SquareGrid(map, squareSize);
+
+		vertices = new List<Vector3>();
+		triangles = new List<int>();
+
+		for (int x = 0; x < squareGrid.squares.GetLength(0); x++)
+		{
+			for (int y = 0; y < squareGrid.squares.GetLength(1); y++)
+			{
+				TriangulateSquare(squareGrid.squares[x, y]);
+			}
+		}
+
+		Mesh mesh = new Mesh();
+		GetComponent<MeshFilter>().mesh = mesh;
+
+		mesh.vertices = vertices.ToArray();
+		mesh.triangles = triangles.ToArray();
+		mesh.RecalculateNormals();
+
+        Debug.Log(vertices.Count);
+        Debug.Log(triangles.Count);
+
+	}
+
+	void TriangulateSquare(Square square)
+	{
+		switch (square.configuration)
+		{
+			case 0:
+				break;
+
+            // 1 points:
+            case 1:
+                MeshFromPoints(square.centreBottom, square.bottomLeft, square.centreLeft);
+                
+                break;
+            case 2:
+                MeshFromPoints(square.centreRight, square.bottomRight, square.centreBottom);
+                Debug.Log(2);
+                break;
+            case 4:
+                MeshFromPoints(square.centreTop, square.topRight, square.centreRight);
+                break;
+            case 8:
+                MeshFromPoints(square.topLeft, square.centreTop, square.centreLeft);
+                break;
+
+            // 2 points:
+            case 3:
+                MeshFromPoints(square.centreRight, square.bottomRight, square.bottomLeft, square.centreLeft);
+                break;
+            case 6:
+                MeshFromPoints(square.centreTop, square.topRight, square.bottomRight, square.centreBottom);
+                break;
+            case 9:
+                MeshFromPoints(square.topLeft, square.centreTop, square.centreBottom, square.bottomLeft);
+                break;
+            case 12:
+                MeshFromPoints(square.topLeft, square.topRight, square.centreRight, square.centreLeft);
+                break;
+            case 5:
+                MeshFromPoints(square.centreTop, square.topRight, square.centreRight, square.centreBottom, square.bottomLeft, square.centreLeft);
+                break;
+            case 10:
+                MeshFromPoints(square.topLeft, square.centreTop, square.centreRight, square.bottomRight, square.centreBottom, square.centreLeft);
+                break;
+
+            // 3 point:
+            case 7:
+                MeshFromPoints(square.centreTop, square.topRight, square.bottomRight, square.bottomLeft, square.centreLeft);
+                break;
+            case 11:
+                MeshFromPoints(square.topLeft, square.centreTop, square.centreRight, square.bottomRight, square.bottomLeft);
+                break;
+            case 13:
+                MeshFromPoints(square.topLeft, square.topRight, square.centreRight, square.centreBottom, square.bottomLeft);
+                break;
+            case 14:
+                MeshFromPoints(square.topLeft, square.topRight, square.bottomRight, square.centreBottom, square.centreLeft);
+                break;
+
+            // 4 point:
+            case 15:
+				MeshFromPoints(square.topLeft, square.topRight, square.bottomRight, square.bottomLeft);
+				break;
+		}
+    }
+
+	void MeshFromPoints(params Node[] points)
+	{
+		AssignVertices(points);
+
+        for (int i = 2; i < points.Length; i++)
+        {
+            CreateTriangle(points[0], points[i-1], points[i]);
+        }
+	}
+
+	void AssignVertices(Node[] points)
+	{
+		for (int i = 0; i < points.Length; i++)
+		{
+			if (points[i].vertexIndex == -1)
+			{
+				points[i].vertexIndex = vertices.Count;
+				vertices.Add(points[i].position);
+			}
+		}
+	}
+
+	void CreateTriangle(Node a, Node b, Node c)
+	{
+		triangles.Add(a.vertexIndex);
+		triangles.Add(b.vertexIndex);
+		triangles.Add(c.vertexIndex);
+	}
+
+	private void OnDrawGizmos()
     {
-        squareGrid = new SquareGrid(map, squareSize);
+        if (squareGrid != null)
+        {
+            for (int x = 0; x < squareGrid.squares.GetLength(0); x++)
+            {
+                for (int y = 0; y < squareGrid.squares.GetLength(1); y++)
+                {
+                    Gizmos.color = (squareGrid.squares[x, y].topLeft.active) ? Color.black : Color.white;
+                    Gizmos.DrawCube(squareGrid.squares[x, y].topLeft.position, Vector3.one * 0.4f);
+                }
+            }
+        }
     }
 
     public class SquareGrid
@@ -15,7 +148,29 @@ public class MeshGenerator : MonoBehaviour
 
         public SquareGrid(bool[,] map, float squareSize)
         {
+            int nodeCountX = map.GetLength(0);
+            int nodeCountY = map.GetLength(1);
+            float mapWidth = nodeCountX * squareSize;
+            float mapHeight = nodeCountY * squareSize;
 
+            ControlNode[,] controlNodes = new ControlNode[nodeCountX, nodeCountY];
+            for (int x = 0; x < nodeCountX; x++)
+            {
+                for (int y = 0; y < nodeCountY; y++)
+                {
+                    Vector3 pos = new Vector3(-mapWidth / 2 + x * squareSize + squareSize / 2, -mapHeight / 2 + y * squareSize + squareSize / 2, 0);
+                    controlNodes[x, y] = new ControlNode(pos, !map[x, y], squareSize);
+                }
+            }
+
+            squares = new Square[nodeCountX - 1, nodeCountY - 1];
+            for (int x = 0; x < nodeCountX - 1; x++)
+            {
+                for (int y = 0; y < nodeCountY - 1; y++)
+                {
+                    squares[x, y] = new Square(controlNodes[x, y + 1], controlNodes[x + 1, y + 1], controlNodes[x + 1, y], controlNodes[x, y]);
+                }
+            }
         }
     }
 
@@ -23,19 +178,29 @@ public class MeshGenerator : MonoBehaviour
     {
         public ControlNode topLeft, topRight, bottomRight, bottomLeft;
         public Node centreTop, centreRight, centreBottom, centreLeft;
+        public int configuration;
 
         public Square (ControlNode _topLeft, ControlNode _topRight, ControlNode _bottomRight, ControlNode _bottomLeft)
         {
-            topLeft = _topLeft;
-            topRight = _topRight;
-            bottomLeft = _bottomLeft;
-            bottomRight = _bottomRight;
+			topLeft = _topLeft;
+			topRight = _topRight;
+			bottomRight = _bottomRight;
+			bottomLeft = _bottomLeft;
 
-            centreTop = topLeft.right;
-            centreLeft = bottomLeft.above;
-            centreRight = bottomRight.above;
-            centreBottom = bottomLeft.right;
-        }
+			centreTop = topLeft.right;
+			centreRight = bottomRight.above;
+			centreBottom = bottomLeft.right;
+			centreLeft = bottomLeft.above;
+
+			if (topLeft.active)
+				configuration += 8;
+			if (topRight.active)
+				configuration += 4;
+			if (bottomRight.active)
+				configuration += 2;
+			if (bottomLeft.active)
+				configuration += 1;
+		}
     }
 
     public class Node
@@ -46,6 +211,7 @@ public class MeshGenerator : MonoBehaviour
         public Node(Vector3 _position)
         {
             position = _position;
+            vertexIndex = -1;
         }
     }
 
@@ -57,8 +223,8 @@ public class MeshGenerator : MonoBehaviour
         public ControlNode(Vector3 _position, bool _active, float squareSize) : base(_position)
         {
             active = _active;
-            above = new Node(_position + Vector3.up * squareSize);
-            right = new Node(_position + Vector3.right * squareSize);
+            above = new Node(_position + Vector3.up * squareSize / 2f);
+            right = new Node(_position + Vector3.right * squareSize / 2f);
         }
     }
 }
